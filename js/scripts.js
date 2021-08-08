cf0120dfe2620363dcd18a3736c4cade72f894f6
Zach Beckwith <zbeckwith6@gmail.com>
2021-08-08 15:03:18 -0500

    Index & Footer Updates

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,7 @@ const isValidPhone = (telephone) => {
     const re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
     return re.test(String(telephone).toLowerCase());
 };
+ 
 
 firstNameInput.isValid = () => !!firstNameInput.value;
 lastNameInput.isValid = () => !!lastNameInput.value;
